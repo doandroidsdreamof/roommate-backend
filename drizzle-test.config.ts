c1124288e97ae5916553f7c92c8eef91531382e7
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2025-12-28 06:15:38 +0300

    broken ci fixedv3

--- drizzle-test.config.ts
+++ drizzle-test.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   schema: './src/database/schema/index.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL_LOCAL_TEST,
+    url: process.env.DATABASE_URL_TEST!,
   },
 });
