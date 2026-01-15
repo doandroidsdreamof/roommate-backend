63bd75047e03ba0e78ced37fdae4bfd5b2fe6d6f
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2026-01-16 01:56:53 +0300

    redis disable config

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,28 +1,38 @@
 import { Injectable, Logger, OnModuleDestroy } from '@nestjs/common';
 import Redis from 'ioredis';
 
-// TODO test error handling after creating global exception filter
-//* encapsulation may works here to protect default implementation
 @Injectable()
 export class RedisService extends Redis implements OnModuleDestroy {
   private readonly logger = new Logger(RedisService.name);
+  private readonly enabled = process.env.REDIS_ENABLED === 'true';
 
   constructor() {
     super({
       host: process.env.REDIS_HOST || 'localhost',
       port: parseInt(process.env.REDIS_PORT || '6379'),
+      lazyConnect: true,
     });
 
-    this.on('error', (err) => this.logger.error('Redis Error:', err));
-    this.on('connect', () => this.logger.log('Redis connected'));
+    if (this.enabled) {
+      this.connect().catch((err) =>
+        this.logger.error('Redis connection failed:', err),
+      );
+      this.on('error', (err) => this.logger.error('Redis Error:', err));
+      this.on('connect', () => this.logger.log('Redis connected'));
+    } else {
+      this.logger.warn('Redis is DISABLED');
+    }
   }
 
   async invalidate(key: string): Promise<void> {
+    if (!this.enabled) return;
     await this.del(key);
     this.logger.debug(`Cache invalidated for key: ${key}`);
   }
 
   async getJSON<T>(key: string): Promise<T | null> {
+    if (!this.enabled) return null;
+
     if (typeof key !== 'string') {
       throw new Error(`Redis key must be a string, received: ${typeof key}`);
     }
@@ -42,6 +52,8 @@ export class RedisService extends Redis implements OnModuleDestroy {
     value: T,
     ttlSeconds: number,
   ): Promise<void> {
+    if (!this.enabled) return;
+
     if (typeof key !== 'string') {
       throw new Error(`Redis key must be a string, received: ${typeof key}`);
     }
@@ -57,6 +69,8 @@ export class RedisService extends Redis implements OnModuleDestroy {
   }
 
   onModuleDestroy() {
-    return this.quit();
+    if (this.enabled) {
+      return this.quit();
+    }
   }
 }
